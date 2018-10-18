b9d3e5a54bf2c1e9845a8ac41148fb08b1fa572c
caguilar187 <caguilar187@gmail.com>
2018-10-18 12:42:51 -0400

    add fuzzwares checking

--- vars/mobileBuildStage.groovy
+++ vars/mobileBuildStage.groovy
@@ -1,8 +1,14 @@
-def call(Closure body) {
+def call(String prettyName = "", Closure body) {
+	def jobName = slack.jobName()
+	def prettyJobName = prettyName
+	if (prettyJobName.trim().equals("")) {
+		prettyJobName = jobName
+	}
 	stage("Build") {
 		body()
 		archiveArtifacts '**/archive/*.ipa, **/output/**/*.apk'
 		storeFuzzArtifacts()
 		slack.buildMessage()
+		build job: 'Fuzzwares Push Notifications', parameters: [string(name: 'JOB', value: jobName), string(name: 'NAME', value: prettyJobName), string(name: 'BUILD', value: "${env.BUILD_NUMBER}")], wait: false
 	}
 }
