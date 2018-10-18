aa3ebe0173a9f303c61c1cde8db0d3d23648cdaa
caguilar187 <caguilar187@gmail.com>
2018-10-18 11:21:30 -0400

    be loud error

--- vars/reportResultsAndCoverage.groovy
+++ vars/reportResultsAndCoverage.groovy
@@ -3,6 +3,6 @@ def call() {
 		junit allowEmptyResults: true, testResults: '**/*.junit'
 		cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/*cobertura.xml', conditionalCoverageTargets: '70, 0, 0', failNoReports: false, failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false							
 	} catch (Exception e) {
-		//Silent Error
+		throw e
 	}
 }
