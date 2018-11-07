83e395dfe25a0c1576905fc3b96350075025a5da
caguilar187 <caguilar187@gmail.com>
2018-11-07 10:31:33 -0500

    allfastlane logic in one file now

--- vars/fastlane.groovy
+++ vars/fastlane.groovy
@@ -46,3 +46,51 @@ def setup(Closure body = null) {
 	install_certs()
 	install_dependencies()
 }
+
+def perform_test(String inKeys = "", Closure body = null) {
+	def keys = inKeys.split(",")
+	if (body != null) {
+		body()
+	}
+	for(key in keys){
+		fastlane "perform_tests key:${key}"
+	}
+}
+
+def performTestStage(String inKeys = "", Closure body = null) {
+	testStage {
+		if (body != null) {
+			body()
+		}
+		perform_test(inKeys)
+		archiveAppForTesting()
+	}
+}
+
+def deploy_jenkins(String inKeys = "", Closure body = null) {
+	def keys = inKeys.split(",")
+	if (body != null) {
+		body()
+	}
+	for(key in keys){
+		fastlane "deploy_jenkins key:${key}"
+	}
+}
+
+def deployStage(String inKeys = "", String appName = "", Boolean release = false, Closure body = null) {
+	mobileBuildStage(appName) {
+		if (body != null) {
+			body()
+		}
+		if (!release) {
+			deploy_jenkins(inKeys) 
+		}
+	}
+}
+
+def pipeline(String inKeys = "", String appName = "", Closure body) {
+	setup()
+	deployStage(inKeys, appName)
+	performTestStage(inKeys)
+	report()
+}
--- vars/fastlaneBuild.groovy
+++
@@ -1,6 +0,0 @@
-def call(String inKeys = "", String appName = "", Closure body) {
-	fastlaneSetup()
-	fastlaneBuild(inKeys, appName)
-	fastlaneTest(inKeys)
-	fastlaneReport()
-}
