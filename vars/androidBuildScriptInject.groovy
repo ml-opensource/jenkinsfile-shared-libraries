387e58136f418f3cc2b64adf63e76a3aa400e5c5
caguilar187 <caguilar187@gmail.com>
2018-11-19 08:23:22 -0500

    fixing error in android injection

--- vars/androidBuildScriptInject.groovy
+++ vars/androidBuildScriptInject.groovy
@@ -1,6 +1,6 @@
 def call(String qualityFile = "quality_six.gradle", String buildscript = "buildscript.gradle") {
     sh "curl -O 'https://jenkins.fuzzhq.com/userContent/jenkins-scripts/android/${qualityFile}'"
-    sh "printf "\n" >> app/build.gradle"
+    sh 'printf "\n" >> app/build.gradle'
     sh "echo \"apply from: '../${qualityFile}'\" >> app/build.gradle"
     sh "curl https://jenkins.fuzzhq.com/userContent/jenkins-scripts/android/${buildscript} >> build.gradle"
 }
