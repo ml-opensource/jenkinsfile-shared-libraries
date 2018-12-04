3d83676c2d5231139ae487ac0f37e2d015fb516f
caguilar187 <caguilar187@gmail.com>
2018-12-04 16:34:30 -0500

    use bash

--- vars/yarn.groovy
+++ vars/yarn.groovy
@@ -31,9 +31,9 @@ def test(Closure body = null) {
 
 def setup(String nodeVersion = 'node', Closure body = null) {
 	stage("Configure Environment") {
-		sh '#!/bin/bash [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"'
-		sh 'printenv'
-		sh "#!/bin/bash sh nvm install ${nodeVersion}"
+		bash '[ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"'
+		bash 'printenv'
+		bash "nvm install ${nodeVersion}"
 		sh 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
 		sh 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
 		sh 'sudo apt-get update && sudo apt-get install yarn -y'
