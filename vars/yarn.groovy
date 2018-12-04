f39ab02497cd4ed866a74b8c3b7b2cb765a65409
caguilar187 <caguilar187@gmail.com>
2018-12-04 15:29:56 -0500

    log to test

--- vars/yarn.groovy
+++ vars/yarn.groovy
@@ -31,6 +31,8 @@ def test(Closure body = null) {
 
 def setup(String nodeVersion = 'node', Closure body = null) {
 	stage("Configure Environment") {
+		sh 'whoami'
+ 		sh 'pwd'
 		sh "nvm install ${nodeVersion}"
 		sh 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
 		sh 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
