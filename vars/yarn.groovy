ef98697e1fa2b5cac259f9474cc13453a4cff368
caguilar187 <caguilar187@gmail.com>
2018-12-04 15:28:40 -0500

    install yarn correctly

--- vars/yarn.groovy
+++ vars/yarn.groovy
@@ -31,7 +31,7 @@ def test(Closure body = null) {
 
 def setup(String nodeVersion = 'node', Closure body = null) {
 	stage("Configure Environment") {
-		sh 'nvm install ${nodeVersion}'
+		sh "nvm install ${nodeVersion}"
 		sh 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
 		sh 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
 		sh 'sudo apt-get update && sudo apt-get install yarn -y'
