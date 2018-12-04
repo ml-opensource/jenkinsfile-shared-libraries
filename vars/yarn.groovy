cc9100eb18347797ee0bb6e2f419e71e839988f4
caguilar187 <caguilar187@gmail.com>
2018-12-04 16:37:35 -0500

    fixing nvm in interactive shell

--- vars/yarn.groovy
+++ vars/yarn.groovy
@@ -31,9 +31,11 @@ def test(Closure body = null) {
 
 def setup(String nodeVersion = 'node', Closure body = null) {
 	stage("Configure Environment") {
-		bash '[ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"'
-		bash 'printenv'
-		bash "nvm install ${nodeVersion}"
+		sh '''#!/bin/bash
+		      [ -s "$HOME/.nvm/nvm.sh" ] && source "$HOME/.nvm/nvm.sh"
+		```
+		sh 'printenv'
+		sh "nvm install ${nodeVersion}"
 		sh 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
 		sh 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
 		sh 'sudo apt-get update && sudo apt-get install yarn -y'
