97e51c4c3dc7ba3f4c59939fdf95b39219a15e68
caguilar187 <caguilar187@gmail.com>
2018-12-04 11:09:18 -0500

    add setup step for yarn

--- vars/yarn.groovy
+++ vars/yarn.groovy
@@ -28,3 +28,14 @@ def test(Closure body = null) {
 		yarn 'test' 
 	}
 }
+
+def setup(Closure body = null) {
+	stage("Configure Environment") {
+		sh 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
+		sh 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
+		sh 'sudo apt-get update && sudo apt-get install yarn'
+		if (body != null) {
+			body()
+		}
+	}
+}
