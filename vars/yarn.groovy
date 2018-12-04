911f8446bff97b46af0586adf67047444acec967
caguilar187 <caguilar187@gmail.com>
2018-12-04 15:55:16 -0500

    use bash

--- vars/yarn.groovy
+++ vars/yarn.groovy
@@ -33,7 +33,8 @@ def setup(String nodeVersion = 'node', Closure body = null) {
 	stage("Configure Environment") {
 		sh 'whoami'
  		sh 'pwd'
-		sh "nvm install ${nodeVersion}"
+		sh "#!/usr/bin/bash 
+		    nvm install ${nodeVersion}"
 		sh 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
 		sh 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
 		sh 'sudo apt-get update && sudo apt-get install yarn -y'
