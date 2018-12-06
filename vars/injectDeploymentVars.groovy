40f6cb3da9ff502be083be3c5048177b29545491
caguilar187 <caguilar187@gmail.com>
2018-12-06 10:56:21 -0500

    injecting more vars for web

--- vars/injectDeploymentVars.groovy
+++ vars/injectDeploymentVars.groovy
@@ -4,11 +4,16 @@ def call(String baseURL) {
 	endpointPrefix = env.BRANCH_NAME
 	if (currentEnv != "production" || currentEnv != "staging" || currentEnv != "dev") {
   		env.CLEAN_ENV = "dev"
+  		env.STANDARD_ENV = false
+	} else {
+		env.STANDARD_ENV = true
 	}
 	if (endpointPrefix == "production") {
 		endpointPrefix = ""
+		env.IS_PRODUCTION = true
 	} else {
 		endpointPrefix = endpointPrefix + "."
+		env.IS_PRODUCTION = false
 	}
 	env.DEPLOY_URL="${endpointPrefix}${env.BASE_URL}"
 }
