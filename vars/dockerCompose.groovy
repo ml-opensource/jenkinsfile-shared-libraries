d08d150f2bf6fb91ebc8bf5b8987486862b3ee8f
caguilar187 <caguilar187@gmail.com>
2018-12-10 17:24:36 -0500

    trim would return false so change it to check for true instead

--- vars/dockerCompose.groovy
+++ vars/dockerCompose.groovy
@@ -2,7 +2,7 @@ def up(String file) {
 	stage("Docker Up") {
 		bash "docker-compose -f ${file} down"
 		volumes = sh(script: "docker volume ls -qf dangling=true", returnStdout: true)
-		if (!volumes.trim()) {
+		if (volumes.trim()) {
 			bash "docker volume rm ${volumes}"
 		}
 		bash "docker-compose -f ${file} up -d --build"
