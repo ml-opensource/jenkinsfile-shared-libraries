b4d13365cdc6fd4dbac17126b93961b9a4590c5f
caguilar187 <caguilar187@gmail.com>
2018-12-10 16:18:53 -0500

    ensure infra is down first

--- vars/dockerCompose.groovy
+++ vars/dockerCompose.groovy
@@ -1,5 +1,6 @@
 def up(String file) {
 	stage("Docker Up") {
+		bash "docker-compose -f ${file} down"
 		volumes = sh(script: "docker volume ls -qf dangling=true", returnStdout: true)
 		bash "docker volume rm ${volumes}"
 		bash "docker-compose -f ${file} up -d --build"
