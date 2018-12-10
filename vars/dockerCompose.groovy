17a98895b1e566b396b16345039e059e2bdde442
caguilar187 <caguilar187@gmail.com>
2018-12-10 16:17:12 -0500

    use varaible to store volumnes

--- vars/dockerCompose.groovy
+++ vars/dockerCompose.groovy
@@ -1,7 +1,7 @@
 def up(String file) {
 	stage("Docker Up") {
 		volumes = sh(script: "docker volume ls -qf dangling=true", returnStdout: true)
-		bash "docker volume rm $(volumes)"
+		bash "docker volume rm ${volumes}"
 		bash "docker-compose -f ${file} up -d --build"
 	}
 }
