3b8ed72519eea0e93004ed19b9ea0caeaa7743c9
caguilar187 <caguilar187@gmail.com>
2018-12-11 10:55:50 -0500

    update for web/ruby reporting

--- vars/reportResultsAndCoverage.groovy
+++ vars/reportResultsAndCoverage.groovy
@@ -1,8 +1,19 @@
 def call(Closure body = null) {
 	try {
-		junit allowEmptyResults: true, testResults: '**/*.junit, **/junit-reports/*.xml, **/test-results/**/*.xml'
-		cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/*cobertura.xml', conditionalCoverageTargets: '70, 0, 0', failNoReports: false, failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false							
+		//Test Reporting
+		junit allowEmptyResults: true, testResults: '**/*.junit, **/junit-reports/*.xml, **/test-results/**/*.xml, **/TEST*.xml'
 	} catch (Exception e) {
 		throw e
+	}
+	try {
+		//Java and Swift
+		cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/*cobertura.xml', conditionalCoverageTargets: '70, 0, 0', failNoReports: false, failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false							
+	} catch (Exception e) {
+	}
+
+	try {
+		//Rcov	
+		step([$class: 'RcovPublisher', targets: []])						
+	} catch (Exception e) {
 	}
 }
--- vars/testStage.groovy
+++ vars/testStage.groovy
@@ -1,5 +1,7 @@
 def call(Closure body) {
 	stage("Test") {
+		sh 'rm -rf reports || echo "Done"'
+		sh 'mkdir reports'
 		body()
 		reportResultsAndCoverage()
 		slack.testMessage()
