004b5d6f147c2f21b507cdf8da6dc0d71e9eeb65
caguilar187 <caguilar187@gmail.com>
2018-12-11 12:28:15 -0500

    ufo commands build out

--- vars/ufo.groovy
+++ vars/ufo.groovy
@@ -1,3 +1,11 @@
-install() {
+def install() {
 	bash "go get -u github.com/fuzz-productions/ufo"
+}
+
+def deploy(String cluster = 'dev', String config = 'default') {
+	if (config == 'default') {
+		slack.bash "ufo deploy --cluster ${cluster}"
+	} else {
+		slack.bash "ufo deploy --cluster ${cluster} --config ${config}"
+	}
 }
