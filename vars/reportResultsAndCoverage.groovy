a960381c21252c653c724b2ddfee4f5470018d98
caguilar187 <caguilar187@gmail.com>
2018-12-13 13:57:29 -0500

    updating cobertura capturing

--- vars/reportResultsAndCoverage.groovy
+++ vars/reportResultsAndCoverage.groovy
@@ -7,7 +7,7 @@ def call(Closure body = null) {
 	}
 	try {
 		//Java and Swift
-		cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/*cobertura.xml', conditionalCoverageTargets: '70, 0, 0', failNoReports: false, failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false							
+		cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/*cobertura.xml,**/*cobertura*.xml', conditionalCoverageTargets: '70, 0, 0', failNoReports: false, failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false							
 	} catch (Exception e) {
 	}
 
