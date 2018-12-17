c17f5741e5d868fef29ff8a39db8e50f0ef16b20
caguilar187 <caguilar187@gmail.com>
2018-12-17 12:44:32 -0500

    add git mirror to ensure all pushes go to a different remote

--- vars/git.groovy
+++ vars/git.groovy
@@ -3,3 +3,17 @@ def clone(String gitUrl) {
 	sh "rm -rf ${folderName} || echo 'Done'"
 	sh "git clone ${gitUrl}"
 }
+
+def mirror(String mirrorURL) {
+	if (github.isPR()) {}
+		sh "git checkout ${env.CHANGE_BRANCH}"
+	} else {
+		sh "git checkout ${env.BRANCH_NAME}"
+	}
+	sh "git remote add github ${mirrorURL}"
+	if (github.isPR()) {}
+		sh "git push github ${env.CHANGE_BRANCH}"
+	} else {
+		sh "git push github ${env.BRANCH_NAME}"
+	}
+}
