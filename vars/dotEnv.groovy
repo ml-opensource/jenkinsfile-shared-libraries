e7da1cb1f4492d13ed41595cbb4a575b22afd348
caguilar187 <caguilar187@gmail.com>
2018-12-18 12:44:35 -0500

    add write permissions

--- vars/dotEnv.groovy
+++ vars/dotEnv.groovy
@@ -1,6 +1,7 @@
 def call(String envFile, String ending = '', Closure body = null) {
 	withCredentials([file(credentialsId: "${envFile}", variable: 'ENVFILE')]) {
 		sh "mv $ENVFILE .env${ending}"
+		sh "chmod 600 .env${ending}"
 		body()
 		sh "rm .env${ending}"
 	}
