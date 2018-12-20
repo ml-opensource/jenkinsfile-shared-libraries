eac20e215132c6484029deb096f83d321a162911
caguilar187 <caguilar187@gmail.com>
2018-12-20 14:50:45 -0500

    prune all

--- vars/dockerCompose.groovy
+++ vars/dockerCompose.groovy
@@ -1,7 +1,7 @@
 def up(String file) {
 	stage("Docker Up") {
 		bash "docker-compose -f ${file} down"
-		bash "docker volume prune --force"
+		bash "docker system prune --all --force"
 		slack.qbash "docker-compose -f ${file} up -d --build"
 	}
 }
@@ -9,6 +9,6 @@ def up(String file) {
 def down(String file) {
 	stage("Docker Down") {
 		bash "docker-compose -f ${file} down"
-		bash "docker volume prune --force"
+		bash "docker system prune --all --force"
 	}
 }
