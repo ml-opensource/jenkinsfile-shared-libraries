a29e807c0744b163484fbf60e264bc000503ab3b
caguilar187 <caguilar187@gmail.com>
2018-12-20 15:14:50 -0500

    quiet bash command

--- vars/bash.groovy
+++ vars/bash.groovy
@@ -1,3 +1,11 @@
 def call(String command) {
     sh "#!/bin/bash\n source ~/.bash_profile \n  ${command}"
 }
+
+def quiet(String command) {
+	try {
+    	sh "#!/bin/bash\n source ~/.bash_profile \n  ${command}"
+	} catch(Throwable t) {
+		
+	}
+}
