191b056ad2e1bbae80d2d5aa49d2c6e18a2b0171
caguilar187 <caguilar187@gmail.com>
2018-12-20 15:21:50 -0500

    add silent to bash

--- vars/bash.groovy
+++ vars/bash.groovy
@@ -2,10 +2,10 @@ def call(String command) {
     sh "#!/bin/bash\n source ~/.bash_profile \n  ${command}"
 }
 
-def quiet(String command) {
+def silent(String command) {
 	try {
     	sh "#!/bin/bash\n source ~/.bash_profile \n  ${command}"
 	} catch(Throwable t) {
-		
+
 	}
 }
