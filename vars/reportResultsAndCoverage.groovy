f672ef75e9f9a691a03b3273528b029acc5d768b
caguilar187 <caguilar187@gmail.com>
2018-12-20 14:45:53 -0500

    add ./ for clover

--- vars/reportResultsAndCoverage.groovy
+++ vars/reportResultsAndCoverage.groovy
@@ -15,7 +15,20 @@ def call(Closure body = null) {
 		//Clover
 		step([
     		$class: 'CloverPublisher',
-    		cloverReportDir: '~/',
+    		cloverReportDir: 'coverage',
+    		cloverReportFileName: 'clover.xml',
+    		healthyTarget: [methodCoverage: 70, conditionalCoverage: 80, statementCoverage: 80], // optional, default is: method=70, conditional=80, statement=80
+    		unhealthyTarget: [methodCoverage: 50, conditionalCoverage: 50, statementCoverage: 50], // optional, default is none
+    		failingTarget: [methodCoverage: 0, conditionalCoverage: 0, statementCoverage: 0]     // optional, default is none
+  		])			
+	} catch (Exception e) {
+	}
+
+	try {
+		//Clover
+		step([
+    		$class: 'CloverPublisher',
+    		cloverReportDir: './',
     		cloverReportFileName: 'clover.xml',
     		healthyTarget: [methodCoverage: 70, conditionalCoverage: 80, statementCoverage: 80], // optional, default is: method=70, conditional=80, statement=80
     		unhealthyTarget: [methodCoverage: 50, conditionalCoverage: 50, statementCoverage: 50], // optional, default is none
