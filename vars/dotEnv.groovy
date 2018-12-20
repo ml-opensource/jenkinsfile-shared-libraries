88a4c1fdf211818411025e255456e7e89cf762ad
caguilar187 <caguilar187@gmail.com>
2018-12-20 13:32:39 -0500

    adding suffix and prefix

--- vars/dotEnv.groovy
+++ vars/dotEnv.groovy
@@ -1,8 +1,26 @@
-def call(String envFile, String ending = '', Closure body = null) {
+def call(String envFile, Closure body = null) {
 	withCredentials([file(credentialsId: "${envFile}", variable: 'ENVFILE')]) {
 		sh "mv $ENVFILE .env${ending}"
 		sh "chmod 600 .env${ending}"
 		body()
 		sh "rm .env${ending}"
+	}
+}
+
+def withSuffix(String envFile, String ending = '', Closure body = null) {
+	withCredentials([file(credentialsId: "${envFile}", variable: 'ENVFILE')]) {
+		sh "mv $ENVFILE .env${ending}"
+		sh "chmod 600 .env${ending}"
+		body()
+		sh "rm .env${ending}"
+	}
+}
+
+def withPrefix(String envFile, String starting = '', Closure body = null) {
+	withCredentials([file(credentialsId: "${envFile}", variable: 'ENVFILE')]) {
+		sh "mv $ENVFILE ${starting}.env"
+		sh "chmod 600 ${starting}.env"
+		body()
+		sh "rm ${starting}.env"
 	}
 }
