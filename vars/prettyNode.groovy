00bd9c6ada3d09d66f69a20330a43920c84a3aa3
caguilar187 <caguilar187@gmail.com>
2019-01-09 14:54:53 -0500

    echo project

--- vars/prettyNode.groovy
+++ vars/prettyNode.groovy
@@ -1,11 +1,20 @@
 def call(String nodeName = "any", Boolean checkoutCode = true, Closure body) {
 	node(nodeName) {
 		prettyPrintDecorator {
+			hasPR()
 			if (checkoutCode) {
 				checkoutStage()
 				clearChanges()
 			}
 			body()
 		}
+	}
+}
+
+@NonCPS
+def hasPR() {
+	projects = currentBuild.rawBuild.project.getParent().getItems()
+	for (project : projects) {
+		sh "echo ${project.toString()}"
 	}
 }
