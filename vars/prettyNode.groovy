1cd562c406a4ef36a1665de44aa35fac73cb43f7
caguilar187 <caguilar187@gmail.com>
2019-01-09 14:33:44 -0500

    echo project

--- vars/prettyNode.groovy
+++ vars/prettyNode.groovy
@@ -1,4 +1,6 @@
 def call(String nodeName = "any", Boolean checkoutCode = true, Closure body) {
+	project = currentBuild.rawBuild.project
+	echo project
 	node(nodeName) {
 		prettyPrintDecorator {
 			if (checkoutCode) {
