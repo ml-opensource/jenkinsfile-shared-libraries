9e2bd740ddb57736409a1c08138a15f79cf4deda
James von Hagel <jvonhagel@fuzzproductions.com>
2019-04-10 14:33:12 -0400

    adding docker volume location for var www app locations

--- vars/reportResultsAndCoverage.groovy
+++ vars/reportResultsAndCoverage.groovy
@@ -60,6 +60,19 @@ def call(Closure body = null) {
 	}
 
 	try {
+		//Clover
+		step([
+    		$class: 'CloverPublisher',
+    		cloverReportDir: '/var/lib/docker/volumes/*var_www_app/_data/',
+    		cloverReportFileName: 'clover.xml',
+    		healthyTarget: [methodCoverage: 70, conditionalCoverage: 80, statementCoverage: 80], // optional, default is: method=70, conditional=80, statement=80
+    		unhealthyTarget: [methodCoverage: 50, conditionalCoverage: 50, statementCoverage: 50], // optional, default is none
+    		failingTarget: [methodCoverage: 0, conditionalCoverage: 0, statementCoverage: 0]     // optional, default is none
+  		])			
+	} catch (Exception e) {
+	}
+
+	try {
 		//Rcov	
 		step([$class: 'RcovPublisher', targets: []])						
 	} catch (Exception e) {
