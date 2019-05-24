e456006085cf02f11cbc7954fb4e5db3ce01f626
caguilar187 <cesar@fuzzproductions.com>
2019-05-24 14:37:46 -0400

    use hudson url instead of hardcoding

--- vars/androidBuildScriptInject.groovy
+++ vars/androidBuildScriptInject.groovy
@@ -20,8 +20,8 @@
  * @return nothing
  */
 def call(String qualityFile = "quality_six.gradle", String buildscript = "buildscript.gradle") {
-    sh "curl -O 'https://jenkins.fuzzhq.com/userContent/jenkins-scripts/android/${qualityFile}'"
+    sh "curl -O '${env.HUDSON_URL}/userContent/jenkins-scripts/android/${qualityFile}'"
     sh 'printf "\n" >> app/build.gradle'
     sh "echo \"apply from: '../${qualityFile}'\" >> app/build.gradle"
-    sh "curl https://jenkins.fuzzhq.com/userContent/jenkins-scripts/android/${buildscript} >> build.gradle"
+    sh "curl ${env.HUDSON_URL}/userContent/jenkins-scripts/android/${buildscript} >> build.gradle"
 }
