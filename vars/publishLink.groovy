bd334ed85521b04c79f935d0d74421e727e58248
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2019-06-27 19:06:31 -0400

    Document the 'rtp' invocation in publishLink.groovy

--- vars/publishLink.groovy
+++ vars/publishLink.groovy
@@ -1,5 +1,5 @@
 /**
- * Send {@link slack#linkMessage Slack} a link to the current build.
+ * Send both {@link slack#linkMessage Slack} and Jenkins a link to the current build.
  * <p>
  *     We use the current value of <code>env.DEPLOY_URL</code> for that
  *     url.
@@ -9,7 +9,15 @@
  * @see injectDeploymentVars#call
  */
 def call() {
-	rtp nullAction: '1', parserName: 'HTML', stableText: "<a href=\"http://${env.DEPLOY_URL}\">http://${env.DEPLOY_URL}</a>"
+    // Publish a Rich Text Publishing message - this will appear on the Build Status page.
+	// See https://jenkins.io/doc/pipeline/steps/rich-text-publisher-plugin/ for syntax
+	rtp(
+			nullAction: '1',
+			parserName: 'HTML',
+			stableText: "<a href=\"http://${env.DEPLOY_URL}\">http://${env.DEPLOY_URL}</a>"
+	)
+
+    // Publish a plain URL to our Slack integration. That'll handle formatting for us.
 	deploymentURL = "http://${env.DEPLOY_URL}"
 	slack.linkMessage(deploymentURL)
 }
