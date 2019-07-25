4dbd3ae42e0cd64ff976e221927cbb4108a024a8
caguilar187 <cesar@fuzzproductions.com>
2019-07-25 11:46:21 -0400

    Add setupNoStage

--- vars/yarn.groovy
+++ vars/yarn.groovy
@@ -96,30 +96,38 @@ def test(String extras = "", Closure body = null) {
  * @see fastlane#setup
  * @see gradlew#setup
  */
-def setup(String nodeVersion = 'node', Closure body = null) {
-	stage("Configure Environment") {
-		bash "nvm install ${nodeVersion}"
-		if (nodeVersion != 'node') {
-			bash "nvm alias default ${nodeVersion}"
+def setup(String nodeVersion = 'node', boolean useClosure = false, Closure body = null) {
+	if (useClosure) {
+		stage("Configure Environment") {
+			setupNoStage(nodeVersion, body)	
 		}
-		bash 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
-		bash 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
-		yarnInstallWorked = false
-		failureCount = 0
-		while (!yarnInstallWorked && failureCount<10) {
-			try {
-				bash 'sudo apt-get update && sudo apt-get install yarn -y'
-				yarnInstallWorked = true
-			} catch(Throwable e) {
-				failureCount++
-				sleep 20
-			}
+	} else {
+		setupNoStage(nodeVersion, body)	
+	}
+}
+
+def setupNoStage(String nodeVersion = 'node', Closure body = null) {
+	bash "nvm install ${nodeVersion}"
+	if (nodeVersion != 'node') {
+		bash "nvm alias default ${nodeVersion}"
+	}
+	bash 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
+	bash 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
+	yarnInstallWorked = false
+	failureCount = 0
+	while (!yarnInstallWorked && failureCount<10) {
+		try {
+			bash 'sudo apt-get update && sudo apt-get install yarn -y'
+			yarnInstallWorked = true
+		} catch(Throwable e) {
+			failureCount++
+			sleep 20
 		}
-		sh 'rm -rf dist || echo "Done"'
-		sh 'rm -rf node_modules || echo "Done"'
-		if (body != null) {
-			body()
-		}
+	}
+	sh 'rm -rf dist || echo "Done"'
+	sh 'rm -rf node_modules || echo "Done"'
+	if (body != null) {
+		body()
 	}
 }
 
@@ -130,6 +138,6 @@ def setup(String nodeVersion = 'node', Closure body = null) {
  * @return nothing
  */
 def installAndSetup(String nodeVersion = 'node') {
-	setup(nodeVersion)
+	setup(nodeVersion, true)
 	install()
 }
