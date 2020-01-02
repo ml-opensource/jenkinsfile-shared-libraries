0824a78ed20c9ed3c45c35162ae6f71f26f058af
Philip Cohn-Cort <cliabhach@gmail.com>
2020-01-02 15:43:20 -0500

    Ignore Lint files when collating Checkstyle XML
    This should help dismiss an annoying error message caused by Jenkins' Checkstyle tool picking up our standard `lint.xml` config file.

--- vars/standardReportArchives.groovy
+++ vars/standardReportArchives.groovy
@@ -18,7 +18,7 @@
 def call() {
 	try {
 		//Android Lint and Lizard
-		checkstyle canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/*lint.xml, **/checkstyle.xml', unHealthy: ''
+		checkstyle canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/*checkstyle.xml', unHealthy: ''
 		pmd canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/*pmd.xml', unHealthy: ''
 		dry canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/*cpd.xml, **/cpdCheck.xml', unHealthy: ''
 		openTasks canComputeNew: false, defaultEncoding: '', excludePattern: '**/Libraries/**, **/Pods/**, **/*.framework/**, **/Xcode.app/**, **/build/**', healthy: '', high: 'FIXME,shit,fuck,suck', ignoreCase: true, low: 'deprecated', normal: 'TODO', pattern: '**/*.swift, **/*.java, **/*.kt, **/*.m, **/*.h, **/*.c', unHealthy: ''
