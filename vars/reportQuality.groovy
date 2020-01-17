58c0fe4ca4add8972e55cec7b1d45292186c0262
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2020-01-17 14:13:59 -0500

    Report: remove 'stage' wrapper from reportQuality.groovy

--- vars/reportQuality.groovy
+++ vars/reportQuality.groovy
@@ -1,34 +1,33 @@
 
 /**
- * Report code quality issues within a basic Stage.
+ * Report code quality issues.
  * <p>
  *     This runs a bunch of reporting tools. Configure them with a String
  *     on env called QUALITY_SERVICES. This is intended as a long-term (yet
  *     still unstable) replacement for {@link testStage#call testStage} and
- *     {@link uatStage#call uatStage}.
+ *     {@link uatStage#call uatStage}. To add it to a Pipeline stage, use
+ *     {@link reportStage#call reportStage}.
  * </p>
  *
  * @author Philip Cohn-Cort (Fuzz)
  * @return nothing
  */
 def call() {
-	stage("Report") {
-		final String services = env.QUALITY_SERVICES
+	final String services = env.QUALITY_SERVICES
 
-		try {
-			println "Services: \n- " + services
+	try {
+		println "Services: \n- " + services
 
-			List toolset = prebuiltQualityToolset.autodetect(services)
+		List toolset = prebuiltQualityToolset.autodetect(services)
 
 
-			recordIssues(
-					aggregatingResults: true,
-					enabledForFailure: true,
-					tools: toolset
-			)
-		} catch (RuntimeException unexpected) {
-			println "One problem: ${unexpected.message}"
-		}
+		recordIssues(
+				aggregatingResults: true,
+				enabledForFailure: true,
+				tools: toolset
+		)
+	} catch (RuntimeException unexpected) {
+		println "One problem: ${unexpected.message}"
 	}
 }
 
