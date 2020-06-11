6e7c2af40c64f4d6241143037d993c11cb55af96
James von Hagel <jvonhagel@fuzzproductions.com>
2020-06-11 13:50:51 -0400

    adding a get command for aws secret manager for rds

--- vars/awsGetSecret.groovy
+++ vars/awsGetSecret.groovy
@@ -28,3 +28,36 @@ def string(String aws_credentials, String secret_id, String aws_region = 'us-eas
         )
     }
 }
+
+/**
+ * Execute an AWS cli command <code>aws secretsmanager get-secret-value</code> 
+ * through shell under the given AWS profile, region, and version of the secret.
+ * <p>
+ *     <a href="https://docs.aws.amazon.com/cli/latest/reference/secretsmanager/index.html">
+ *         There are more details options and information regarding the 
+ *         aws secretsmanager get-secret-value cli command.
+ *     </a>
+ * </p>
+ *
+ * @param aws_credentials is the aws credintails that are stored in jenkins
+ * @param secretId is the aws id of the secret
+ * @param secret_key is key value within the rds secret:
+ * username, password, engine, host, port, dbname, dbInstanceIdentifier
+ * @param aws_region is the aws region that the secret resides
+ * @param version_stage is the secret version 
+ * @return the stdout of the shell script
+ */
+def rds(String aws_credentials, String secret_id, String secret_key, String aws_region = 'us-east-1', String version_stage = 'AWSCURRENT') {
+    awsCreds("${aws_credentials}") {
+    sh(
+        script: "aws secretsmanager get-secret-value \
+        --secret-id ${secret_id} \
+        --version-stage ${version_stage} \
+        --region ${aws_region} \
+        --query SecretString \
+        --output text \
+        | jq '${secret_key}' \
+        | tr -d '\n'",
+        returnStdout: true
+    )
+}
